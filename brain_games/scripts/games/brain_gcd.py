7452854fb488d48551acea19f0d5105c82eef069
fiftinmen <fiftin.men@gmail.com>
2024-03-17 19:24:23 +0300

    rename find_greatest_common_divisor() to calc_greatest_common_divisor()

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -10,7 +10,7 @@ def generate_pair_of_numbers():
     return str(random.randint(1, 100))+' '+str(random.randint(1, 100))
 
 
-def find_greatest_common_divisor(pair_of_numbers: str) -> int:
+def calc_greatest_common_divisor(pair_of_numbers: str) -> int:
     numbers = [int(n) for n in pair_of_numbers.split(' ')]
     while numbers[0] != numbers[1]:
         if numbers[0] > numbers[1]:
