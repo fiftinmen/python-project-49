63683f6aaf62b972eb88c734cbe49f051c8a6bf8
fiftinmen <fiftin.men@gmail.com>
2024-03-18 07:46:53 +0300

    rename brain_even_input() to yes_or_no_input() and move it to functions.py; move generate_number() to functions.py

--- brain_games/scripts/games/functions.py
+++ brain_games/scripts/games/functions.py
@@ -1,5 +1,23 @@
 import prompt
+import random
 
 
 def integer_input():
     return prompt.integer(prompt='Your answer: ', empty=False)
+
+
+def generate_number():
+    return random.randint(-1000, 1000)
+
+
+def generate_natural_number():
+    return random.randint(1, 1000)
+
+
+def yes_no_input():
+    pattern = r'(yes)|(no)'
+    answer = prompt.regex(pattern, prompt='Your answer: ', empty=False)
+    answer = answer.groups()
+    # получили массив из двух элементов:
+    # ['yes', Nonetype] или [NoneType, 'no] в зависимости от ответа
+    return answer[0] if answer[0] else answer[1]
