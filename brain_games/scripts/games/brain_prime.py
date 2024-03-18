8f90a45978ae495af5872adee6a328892ab95918
fiftinmen <fiftin.men@gmail.com>
2024-03-18 07:43:23 +0300

    add new game - brain_prime: guess if the number is prime or not

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from . import brain_engine
+from . import functions
+
+
+def prime_yes_or_no(number: int) -> str:
+    for i in range(2, number // 2+1):
+        if number % i == 0:
+            return 'no'
+    return 'yes'
+
+
+rules = 'Answer "yes" if the number is prime, otherwise answer "no".'
+
+
+def main():
+    brain_engine.game(rules=rules,
+                      input_function=functions.yes_no_input,
+                      question_generator=functions.generate_natural_number,
+                      correct_answer_generator=prime_yes_or_no)
+
+
+if __name__ == '__main__':
+    main()
