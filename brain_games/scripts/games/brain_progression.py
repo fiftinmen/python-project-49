a5c2c82cf27afa389de7c499449354aebe61a1c3
fiftinmen <fiftin.men@gmail.com>
2024-03-18 15:24:58 +0300

    refactoring and fix code; replace magic numbers with constants; refactor generate_progression(); fix hidden element marker and remove , in progression sequence;

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -6,34 +6,38 @@ from .. import brain_engine
 from .. import functions
 
 
-def generate_progression():
-    MAX_PROGRESSION_STEP = 30
-    MIN_PROGRESSION_LENGTH = 5
-    MAX_START_NUMBER = 50
+HIDDEN_ELEMENT_MARKER = '..'
+MAX_PROGRESSION_STEP = 30
+MIN_PROGRESSION_LENGTH = 5
+MAX_PROGRESSION_LENGTH = 10
+VAR_PROGRESSION_LENGTH = MAX_PROGRESSION_LENGTH - MIN_PROGRESSION_LENGTH
+MAX_START_NUMBER = 50
 
-    progression = ''
-    progression_length = random.randint(0, 4)+MIN_PROGRESSION_LENGTH
-    missing_element = random.randint(1, progression_length-1)
+
+def generate_progression():
+    progression = []
+    progression_length = random.randint(0, VAR_PROGRESSION_LENGTH) + \
+        MIN_PROGRESSION_LENGTH
+    missing_element = random.randint(1, progression_length-2)
     number = random.randint(0, MAX_START_NUMBER)
     progression_step = random.randint(1, MAX_PROGRESSION_STEP)
     for i in range(0, progression_length):
         if i != missing_element:
-            progression += f'{str(number)}, '
+            progression.append(str(number))
         else:
-            progression += '..., '
+            progression.append(HIDDEN_ELEMENT_MARKER)
         number += progression_step
-    progression += str(number)
-    return progression
+    return ' '.join(progression)
 
 
 def find_missing_element(sequence: str) -> int:
     for i, el in enumerate(sequence):
-        if el == '...':
+        if el == HIDDEN_ELEMENT_MARKER:
             return i
 
 
 def calc_missing_element(progression: str) -> int:
-    elements = [n for n in progression.split(', ')]
+    elements = [n for n in progression.split(' ')]
     missing_element_index = find_missing_element(elements)
     return (int(elements[missing_element_index-1]) +
             int(elements[missing_element_index + 1])) // 2
