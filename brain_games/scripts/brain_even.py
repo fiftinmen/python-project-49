49d965d251f4f464279798b0cd5d6c609e0547ef
fiftinmen <fiftin.men@gmail.com>
2024-04-02 17:56:05 +0300

    fix call game in game-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.game_engine import game
 from brain_games.games.even_game import brain_even
 
 
 def main():
-    game(**brain_even())
+    brain_even()
 
 
 if __name__ == '__main__':
