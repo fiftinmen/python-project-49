775c7e4f0509db0526b68c06e43e28915a192e20
fiftinmen <fiftin.men@gmail.com>
2024-04-23 14:27:08 +0300

    fix is_prime function

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,20 +5,24 @@ import random
 GAME_PROMPT = 'Answer "yes" if given number is prime. '\
     'Otherwise answer "no".'
 MAX_NUMBER = 100
-FIRST_NOT_PRIME = 4
 FIRST_PRIME = 2
+FIRST_NATURAL_NUMBER = 1
 
 
 def is_prime(number: int) -> str:
-    if number < FIRST_NOT_PRIME:
+    if number < FIRST_PRIME:
         return False
     max_possible_divisor = int(sqrt(number))
+    if max_possible_divisor < FIRST_PRIME:
+        return True
     return all(
-        number % i != 0 for i in range(FIRST_PRIME, max_possible_divisor + 1))
+        number % i != 0
+        for i in range(FIRST_PRIME, max_possible_divisor + 1)
+        )
 
 
 def generate_question_and_answer():
-    question = random.randint(0, MAX_NUMBER)
+    question = random.randint(FIRST_NATURAL_NUMBER, MAX_NUMBER)
     answer = 'yes' if is_prime(question) else 'no'
     return question, answer
 
